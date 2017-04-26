ef07bb50737ae2d4d931fc8054c4c0cf94bc61d7
kelvin knigton <keltheceo@gmail.com>
2017-04-26 11:35:01 -0700

    1st awesome commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  @Input() list: string;
+
+  constructor() {
+    this.list = 'Super hero list';
+  }
+
+  ngOnInit() {
+  }
+
 }
